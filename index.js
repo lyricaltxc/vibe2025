18e30e65ab6f61f531ca4467cf5999d848e4e27c
lyricaltxc <stroev.vanya2016@yandex.ru>
2025-06-19 20:42:59 +0300

    fix kovichka

--- index.js
+++ index.js
@@ -83,4 +83,4 @@ async function handleRequest(req, res) {
 }
 
 const server = http.createServer(handleRequest);
-server.listen(PORT, () => console.log('Server running on port ${PORT}'));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
